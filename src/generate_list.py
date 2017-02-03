e080070458fc1fc9bb3e3ab3c5db34b314e67580
icecowface <ni.natchapol_st@tni.ac.th>
2017-02-03 02:41:37 +0000

    Commit4 - generate_list v2

--- src/generate_list.py
+++ src/generate_list.py
@@ -1,6 +1,8 @@
 import random
 def generate_list():
     alist = [x for x in range(random.randint(-10,10))]
+    assert (len(alist)!=0), "alist not have value in array"
+    assert (sum(alist)>0), "sum of alist < 0"
     return alist
     
 def printIt():
@@ -12,3 +14,7 @@ def main():
 if __name__ == '__main__':
     print("Test printIt():")
     main()
+    
+    
+
+    
